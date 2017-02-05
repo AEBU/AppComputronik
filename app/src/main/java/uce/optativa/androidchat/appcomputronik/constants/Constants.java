25f95faf7e2a04d9333b54be6b3898ef571e909d
Alexis Bautista <rhack_100@outlook.com>
2017-02-04 22:46:28 -0500

    :2 Login Con Retrofit

---
+++ app/src/main/java/uce/optativa/androidchat/appcomputronik/constants/Constants.java
@@ -0,0 +1,9 @@
+package uce.optativa.androidchat.appcomputronik.constants;
+
+/**
+ * Created by Alexis on 16/01/2017.
+ */
+
+public class Constants {
+    public static final String  url = "http://10.3.3.101:8080/app/";
+}
---
+++ app/src/main/java/uce/optativa/androidchat/appcomputronik/service/UserService.java
@@ -0,0 +1,32 @@
+package uce.optativa.androidchat.appcomputronik.service;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+import uce.optativa.androidchat.appcomputronik.model.User;
+
+/**
+ * Created by Alexis on 15/01/2017.
+ */
+
+public interface UserService {
+
+    @GET("/user/")
+    Call<List<User>> getUsers();
+
+    //@GET("user/{user_id}")
+    //void getUser(@Path("user_id") String user);
+    @GET("user/{user_id}")
+    Call<User> getUser(@Path("user_id") String user);
+
+    @GET("user/{username}/{email}")
+    Call<User> getUserByEmailAndUserName(@Path("username") String username, @Path("email") String email);
+
+    @GET("user/1")
+    void getUser(Callback<List<User>> callback);
+
+
+}
